a99e1da416b0c4bfdabaead82f579995932e61f7
duonghoangkha01 <duonghoangkha2001@gmail.com>
2025-11-26 01:17:41 +0700

    Added Program.cs with entry point for the application

---
+++ samples/EFCore.GenericRepository.Sample/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EFCore.GenericRepository.Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World from EFCore.GenericRepository.Sample!");
+        }
+    }
+}
